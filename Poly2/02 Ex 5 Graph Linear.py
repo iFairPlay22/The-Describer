80eea58c696d78571b6fa374f9984c1207c38482
Lucas Billard <46447981+Azukiro@users.noreply.github.com>
2021-11-23 23:14:15 +0100

    Update 02 Ex 5 Graph Linear.py

--- Poly2/02 Ex 5 Graph Linear.py
+++ Poly2/02 Ex 5 Graph Linear.py
@@ -1,18 +1,26 @@
 import torch, numpy, matplotlib.pyplot as plt
 
-layer = torch.nn.Linear(1,1)	# creation de la couche Linear
-activ = torch.nn.ReLU()         # fonction d’activation ReLU
-Lx = numpy.linspace(-2,2,50)    # échantillonnage de 50 valeurs dans [-2,2]
-Ly = []
+k = 50
+n = 1
+m = 3
+o = 1
 
-#eval
-for x in Lx:
-  input = torch.FloatTensor([x])	# création d’un tenseur de taille 1
-  v1 = layer(input)			        # utilisation du neurone
-  v2 = activ(v1)			        # application de la fnt activation ReLU
-  Ly.append(v2.item())		        # on stocke le résultat dans la liste
+layer = torch.nn.Linear(n,m)
+layer2 = torch.nn.Linear(m,o)	# creation de la couche Linear
+
+activ = torch.nn.ReLU()         # fonction d’activation ReLU
+
+Lx = numpy.linspace(-2,2,k)    # échantillonnage de 50 valeurs dans [-2,2]
+Lx = Lx.reshape(k,n)
+
+input = torch.FloatTensor(Lx)
+v1 = layer(input)			        # utilisation du neurone
+v2 = activ(v1)
+v2 = layer2(v2)
+
+Ly = v2.detach().numpy()		        # application de la fnt activation ReLU
 
 # tracé
-plt.plot(Lx,Ly,'.') 	# dessine un ensemble de points
+plt.plot(Lx,Ly) 	# dessine un ensemble de points
 plt.axis('equal') 		# repère orthonormé
 plt.show() 			    # ouvre la fenêtre d'affichage
