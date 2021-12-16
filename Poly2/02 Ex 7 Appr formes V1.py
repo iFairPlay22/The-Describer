150f48eadbaf8ae33f835d37252486604ef77c9e
iFairPlay22 <ewen.bouquet@gmail.com>
2021-12-16 10:12:54 +0100

    Ex 7

--- Poly2/02 Ex 7 Appr formes V1.py
+++ Poly2/02 Ex 7 Appr formes V1.py
@@ -2,39 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import torch
-import torch.nn.functional as FNT
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as O
 
 ######################################################
 
 # (x,y, category)
-points = [ [(0.5,0.4),0],
-           [(0.8,0.3),0],
-		    [(0.3,0.8),0],
-		    [(-.4,0.3),1],
-		    [(-.3,0.7),1],
-		    [(-.7,0.2),1],
-		    [(-.4,-.5),1],
-		    [(0.7,-.4),2],
-		    [(0.5,-.6),2]]
+points = [
+    [(0.5, 0.4), 0],
+    [(0.8, 0.3), 0],
+    [(0.3, 0.8), 0],
+    [(-.4, 0.3), 1],
+    [(-.3, 0.7), 1],
+    [(-.7, 0.2), 1],
+    [(-.4, -.5), 1],
+    [(0.7, -.4), 2],
+    [(0.5, -.6), 2]
+]
 
 ######################################################
 #
 #  outils d'affichage -  NE PAS TOUCHER
 
-def DessineFond():
-    iS = ComputeCatPerPixel()
+
+def DessineFond(model):
+    iS = ComputeCatPerPixel(model)
     levels = [-1, 0, 1, 2]
     c1 = ('r', 'g', 'b')
-    plt.contourf(XXXX, YYYY, iS, levels, colors = c1)
+    plt.contourf(XXXX, YYYY, iS, levels, colors=c1)
+
 
 def DessinePoints():
-    c2 = ('darkred','darkgreen','lightblue')
+    c2 = ('darkred', 'darkgreen', 'lightblue')
     for point in points:
         coord = point[0]
-        cat   = point[1]
-        plt.scatter(coord[0], coord[1] ,  s=50, c=c2[cat],  marker='o')
+        cat = point[1]
+        plt.scatter(coord[0], coord[1],  s=50, c=c2[cat],  marker='o')
 
-XXXX , YYYY = np.meshgrid(np.arange(-1, 1, 0.01), np.arange(-1, 1, 0.01))
+
+XXXX, YYYY = np.meshgrid(np.arange(-1, 1, 0.01), np.arange(-1, 1, 0.01))
 
 
 ##############################################################
@@ -63,17 +70,70 @@ XXXX , YYYY = np.meshgrid(np.arange(-1, 1, 0.01), np.arange(-1, 1, 0.01))
 #          permet de ne pas tenir compte de cet écart négatif dans l'erreur.
 
 
-def ComputeCatPerPixel():
-    s = XXXX.shape
-    CCCC = torch.ones(s)
-    return CCCC
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self._inputN = 2
+        self._outputM = 3
+        self._layer1 = torch.nn.Linear(self._inputN, self._outputM)
 
-iteration = 0
+    def forward(self, allCoords):
+        return self._layer1(allCoords)
 
-DessineFond()
-DessinePoints()
 
-plt.title(str(iteration))
-plt.show(block=False)
-plt.pause(2)  # pause avec duree en secondes
+def Error(predictedScores, correctScores):
+    res = torch.FloatTensor([0])
+    for j in range(3):
+        goodCategScore = torch.diagonal(predictedScores[:, correctScores])
+        currentCategScore = predictedScores[:, j]
+        diff = torch.sub(currentCategScore, goodCategScore)
+        positiveDiff = torch.max(torch.tensor([0]), diff)
+        res += torch.sum(positiveDiff)
+    return res
 
+
+def ComputeCatPerPixel(model):
+
+    allCoords = torch.zeros(XXXX.shape[0], XXXX.shape[1], 2)
+    allCoords[:, :, 0] = torch.FloatTensor(XXXX)
+    allCoords[:, :, 1] = torch.FloatTensor(YYYY)
+
+    allScores = model.forward(allCoords)
+    allMaxScores = torch.argmax(allScores, dim=2)
+    allMaxScoresNa = allMaxScores.detach().numpy()
+
+    return allMaxScoresNa
+
+
+model = Net()
+optim = O.SGD(model.parameters(), lr=0.5)
+
+pointsTensor = torch.FloatTensor([list(x[0]) for x in points])
+categoriesTensor = torch.tensor([x[1] for x in points])
+
+for iteration in range(1000):
+
+    # Calcul
+    optim.zero_grad()  # remet à zéro le calcul du gradient
+    predictions = model.forward(pointsTensor)  # démarrage de la passe Forward
+
+    # predictions = np.argmax(predictions, dim=2)
+
+    # choisit une function de loss de PyTorch
+    ErrTot = Error(predictions, categoriesTensor)
+    ErrTot.backward()  # effectue la rétropropagation
+    optim.step()  # algorithme de descente
+
+    # Affichage
+    print("Iteration : ", iteration, " ErrorTot : ", ErrTot.item())
+    DessineFond(model)  # dessine les zones
+    DessinePoints()  # dessine les données sous forme de points
+    plt.title(str(iteration))  # insère le n° de l’itération dans le titre
+    plt.pause(2)  # pause en secondes pour obtenir un refresh
+    plt.show(block=False)  # affichage sans blocage du thread
+
+    if (ErrTot.item() == 0.0):
+        print("On a convergé ! :)")
+        break
+
+plt.pause(2)
