ff31ebb266b74a7e8573f6d61530e8bab368147a
Ewen Bouquet <ewen.bouquet@gmail.com>
2022-06-02 16:17:48 +0200

    LOL

--- projet/frontend/web/describe-image/src/App.vue
+++ projet/frontend/web/describe-image/src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-button type="primary">Primary</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
