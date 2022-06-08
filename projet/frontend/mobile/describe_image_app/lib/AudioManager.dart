62958608e25abbf718980f2853ccc0a171f68939
Lucas Billard <46447981+Azukiro@users.noreply.github.com>
2022-06-08 15:38:03 +0200

    Update flutter

--- projet/frontend/mobile/describe_image_app/lib/AudioManager.dart
+++ projet/frontend/mobile/describe_image_app/lib/AudioManager.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_tts/flutter_tts.dart';
-
+import 'dart:io' show Platform;
 class AudioManager {
   AudioManager._() {
-    FlutterTts().setLanguage("en-us");
+ 
+    FlutterTts().setLanguage(Platform.localeName);
   }
 
   static final AudioManager _instance = AudioManager._();
