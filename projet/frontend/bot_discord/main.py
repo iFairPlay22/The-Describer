e594b113eaa75bb5be2ab3a9288b4ba321cb132f
Fabinours <44270057+Fabinours@users.noreply.github.com>
2022-06-08 10:40:50 +0200

    Bot discord w. language support

--- projet/frontend/bot_discord/main.py
+++ projet/frontend/bot_discord/main.py
@@ -12,6 +12,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Init Bot
 bot = commands.Bot(command_prefix='!')
 bot.enable_automatic_description = True
+bot.language = "EN"
+bot.supported_languages = ["BG", "CS", "DA", "DE", "EL", "ES", "FI", "FR", "HU",
+                               "ID", "IT", "JA", "LT", "LV", "NL", "PL", "PT-PT", "PT-BR", "RO", "RU", "SK", "SL", "SV", "TR", "ZH"]
 
 # Command test
 @bot.command(name='hi', help='Say hi', scope=847072561816141884,)
@@ -52,6 +55,8 @@ async def on_message(message):
     elif message.content.startswith('!about'):
         embedMsg = discord.Embed(title="About me :", description="", color=0xff0000)
         embedMsg.add_field(name="Website", value="https://the-describers.netlify.app/", inline=False)
+        embedMsg.add_field(name="Github", value="https://github.com/iFairPlay22/DescribeImage", inline=False)
+        embedMsg.add_field(name="Supported languages", value="BG, CS, DA, DE, EL, ES, FI, FR, HU, ID, IT, JA, LT, LV, \nNL, PL, PT-PT, PT-BR, RO, RU, SK, SL, SV, TR, ZH", inline=False)
         embedMsg.add_field(name="Authors", value="• Fabien Courtois\n• Loïc Fournier\n• Lucas Billard\n• Ewen Bouquet", inline=False)
         await message.reply(embed = embedMsg)
 
@@ -62,8 +67,20 @@ async def on_message(message):
         embedMsg.add_field(name="disable", value="Disable automatic image description", inline=False)
         embedMsg.add_field(name="enable", value="Enable automatic image description", inline=False)
         embedMsg.add_field(name="about", value="Everything you have to know about me", inline=False)
+        embedMsg.add_field(name="language", value="Switch to specified language", inline=False)
         embedMsg.add_field(name="commands", value="List every commands available", inline=False)
         await message.reply(embed = embedMsg)
+    # !language : Switch to specified language 
+    elif message.content.startswith('!language'):
+        tab = message.content.split(" ")
+        if(len(tab) == 2 and tab[1] in bot.supported_languages):
+            bot.language = tab[1]
+            embedMsg = discord.Embed(title="", description="Language changed to " + bot.language, color=0xff0000)
+            await message.reply(embed = embedMsg)
+        else:
+            embedMsg = discord.Embed(title="", description="", color=0xff0000)
+            embedMsg.add_field(name="Supported languages", value="BG, CS, DA, DE, EL, ES, FI, FR, HU, ID, IT, JA, LT, LV, \nNL, PL, PT-PT, PT-BR, RO, RU, SK, SL, SV, TR, ZH", inline=False)
+            await message.reply(embed = embedMsg)
     else:
         return
 
@@ -71,10 +88,10 @@ async def on_message(message):
 async def request_from_url(message, url):
     await message.channel.send('Processing ...')
 
-    res = requests.post('https://www.loicfournier.fr/iadecode/from_url', json={'file': url})
+    res = requests.post('https://loicfournier.fr/iadecode/from_url/'+bot.language, json={'file': url})
         
     if(res.status_code == 200):
-        embedMsg = discord.Embed(title="", description=res.json()['message'], color=0xff0000)
+        embedMsg = discord.Embed(title="", description=res.json()['message'].capitalize(), color=0xff0000)
         await message.reply(embed = embedMsg)
     else:
         embedMsg = discord.Embed(title="", description="Error : verify your url", color=0xff0000)
